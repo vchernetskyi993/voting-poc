95c8fbf937734f07909f8fd61a4b25ccc103db3f
Vsevolod Chernetskyi <vchernetskiy@mail.ua>
2022-04-06 19:36:27 +0300

    added part of voting tests

--- blockchain/avalanche/voting-poc/contracts/contracts/Elections.sol
+++ blockchain/avalanche/voting-poc/contracts/contracts/Elections.sol
@@ -12,14 +12,17 @@ contract Elections is Ownable {
         string[] candidates;
     }
 
-    uint256 private lastElectionId;
+    struct CandidateVotes {
+        uint256 candidateId;
+        uint256 votes;
+    }
+
+    uint256 public lastElectionId;
     Election[] private elections;
+    mapping(uint256 => mapping(uint256 => uint256)) private results;
 
     event ElectionCreated(uint256 electionId, Election election);
-
-    function vote(uint256 electionId, uint256 candidateId) external {
-        // TODO: implement
-    }
+    event Voted(uint256 electionId, uint256 candidateId);
 
     function createElection(Election calldata election) external onlyOwner {
         require(
@@ -31,7 +34,36 @@ contract Elections is Ownable {
         emit ElectionCreated(lastElectionId++, election);
     }
 
-    function getElection(uint256 electionId) external view returns (Election memory) {
+    function getElection(uint256 electionId)
+        external
+        view
+        returns (Election memory)
+    {
         return elections[electionId];
     }
+
+    function vote(uint256 electionId, uint256 candidateId) external {
+        require(
+            electionId >= 0 && electionId <= elections.length,
+            "Election id invalid"
+        );
+        Election memory election = elections[electionId];
+        require(
+            candidateId >= 0 && candidateId < election.candidates.length,
+            "Candidate id invalid"
+        );
+        require(
+            block.timestamp >= election.start,
+            "Election hasn't started yet"
+        );
+        require(block.timestamp < election.end, "Election has already ended");
+
+        emit Voted(electionId, candidateId);
+    }
+
+    function getVotingResults(uint256 electionId)
+        external
+        view
+        returns (CandidateVotes[] memory)
+    {}
 }
--- blockchain/avalanche/voting-poc/contracts/test/elections.ts
+++ blockchain/avalanche/voting-poc/contracts/test/elections.ts
@@ -1,11 +1,13 @@
 import { expect, use } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import dayjs, { Dayjs } from "dayjs";
-import { ContractReceipt } from "ethers";
+import duration from "dayjs/plugin/duration";
+import { ContractReceipt, Event } from "ethers";
 import { ethers } from "hardhat";
 import { Elections } from "../typechain";
 
 use(chaiAsPromised);
+dayjs.extend(duration);
 
 type Election = {
   start: number;
@@ -17,6 +19,7 @@ type Election = {
 
 describe("Elections contract", () => {
   let contract: Elections;
+  let currentDate = dayjs();
 
   before(async () => {
     const electionsFactory = await ethers.getContractFactory("Elections");
@@ -24,19 +27,23 @@ describe("Elections contract", () => {
     await contract.deployed();
   });
 
-  it("Should generate new ids", async () => {
+  it("Should increment election id", async () => {
+    const electionId = await contract.lastElectionId();
+
     return Promise.all([
-      contract
-        .createElection(election())
-        .then((r) => r.wait())
-        .then(getElectionId),
-      contract
-        .createElection(election())
-        .then((r) => r.wait())
-        .then(getElectionId),
-    ]).then(([first, second]) => {
-      expect(first).to.be.not.equal(second);
-    });
+      expect(
+        contract
+          .createElection(election())
+          .then((r) => r.wait())
+          .then(getElectionId)
+      ).to.eventually.equal(electionId),
+      expect(
+        contract
+          .createElection(election())
+          .then((r) => r.wait())
+          .then(getElectionId)
+      ).to.eventually.equal(electionId.add(1)),
+    ]);
   });
 
   it("Should emit created election", () => {
@@ -51,13 +58,13 @@ describe("Elections contract", () => {
 
   it("Should throw if start is before now", () => {
     return expect(
-      contract.createElection(election(dayjs().subtract(1, "day")))
+      contract.createElection(election(currentDate.subtract(1, "day")))
     ).to.be.rejectedWith(/in the future/);
   });
 
   it("Should throw if end is less than start", () => {
     return expect(
-      contract.createElection(election(dayjs().add(3, "day")))
+      contract.createElection(election(currentDate.add(3, "day")))
     ).to.be.rejectedWith(/before/);
   });
 
@@ -81,9 +88,81 @@ describe("Elections contract", () => {
     );
   });
 
+  it("Should emit event on each vote", async () => {
+    const candidateId = 1;
+    const electionId = await contract
+      .createElection(election())
+      .then((r) => r.wait())
+      .then(getElectionId);
+    await startElection();
+
+    const [actualElectionId, actualCandidateId] = await contract
+      .vote(electionId, candidateId)
+      .then((r) => r.wait())
+      .then(getVotedEvent);
+
+    expect(actualElectionId).to.equal(electionId);
+    expect(actualCandidateId).to.equal(candidateId);
+  });
+
+  it("Should throw for non-existent election", async () => {
+    const lastElectionId = await contract.lastElectionId();
+    return expect(contract.vote(lastElectionId.add(1), 1)).to.be.rejectedWith(
+      /id invalid/
+    );
+  });
+
+  it("Should throw for non-existent candidate", async () => {
+    const electionId = await contract
+      .createElection(election())
+      .then((r) => r.wait())
+      .then(getElectionId);
+    await startElection();
+
+    return expect(contract.vote(electionId, 2)).to.be.rejectedWith(
+      /id invalid/
+    );
+  });
+
+  it("Should prohibit to vote before election start", async () => {
+    const electionId = await contract
+      .createElection(election())
+      .then((r) => r.wait())
+      .then(getElectionId);
+
+    return expect(contract.vote(electionId, 1)).to.be.rejectedWith(/started/);
+  });
+
+  it("Should prohibit to vote after election end", () => {
+    throw new Error("Not implemented!");
+  });
+
+  it("Any user should be able to vote", async () => {
+    const candidateId = 1;
+    const electionId = await contract
+      .createElection(election())
+      .then((r) => r.wait())
+      .then(getElectionId);
+    const [, user] = await ethers.getSigners();
+    await startElection();
+
+    await contract
+      .connect(user)
+      .vote(electionId, candidateId)
+      .then((r) => r.wait());
+
+    const [first, second] = await contract.getVotingResults(electionId);
+    expect(first.votes).to.equal(0);
+    expect(second.votes).to.equal(1);
+  });
+
+  it("Should prohibit to vote more then once", () => {
+    throw new Error("Not implemented!");
+  });
+
   function election(
-    start: Dayjs = dayjs().add(1, "day"),
-    end: Dayjs = dayjs().add(2, "day")
+    start: Dayjs = currentDate.add(1, "day"),
+    end: Dayjs = currentDate.add(2, "day")
   ): Election {
     return {
       start: start.unix(),
@@ -107,12 +186,26 @@ describe("Elections contract", () => {
   }
 
   function getElectionId(receipt: ContractReceipt): number {
-    return receipt.events?.find((e) => e.event === "ElectionCreated")?.args
-      ?.electionId;
+    return getEvent(receipt, "ElectionCreated").args?.electionId;
   }
 
   function getElection(receipt: ContractReceipt): Election {
-    return receipt.events?.find((e) => e.event === "ElectionCreated")?.args
-      ?.election;
+    return getEvent(receipt, "ElectionCreated").args?.election;
+  }
+
+  function getVotedEvent(receipt: ContractReceipt): [number, number] {
+    return getEvent(receipt, "Voted").args as [number, number];
+  }
+
+  function getEvent(receipt: ContractReceipt, type: string): Event {
+    return receipt.events?.find((e) => e.event === type)!;
+  }
+
+  async function startElection(): Promise<void> {
+    const startElectionDuration = dayjs.duration({ days: 1, hours: 1 });
+    await ethers.provider.send("evm_increaseTime", [
+      startElectionDuration.asSeconds(),
+    ]);
+    currentDate = currentDate.add(startElectionDuration);
   }
 });
