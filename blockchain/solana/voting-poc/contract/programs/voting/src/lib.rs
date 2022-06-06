739bfca0c96607b7b0495c6a116ed9df31e347b7
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2022-06-06 19:28:12 +0300

    solana - voting - register org instruction wip

--- blockchain/solana/voting-poc/contract/programs/voting/src/lib.rs
+++ blockchain/solana/voting-poc/contract/programs/voting/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use sha2::{Sha256, Digest};
 
 declare_id!("34jePpDQgnfeJkADMwDTKoCjR9pRtcDS6MMqzdkhXkzT");
 
@@ -7,9 +8,108 @@ pub mod voting {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.main_data.owner = ctx.accounts.owner.key();
+        Ok(())
+    }
+
+    pub fn register_organization(ctx: Context<RegisterOrganization>, _organization: Pubkey) -> Result<()> {
+        require_keys_eq!(ctx.accounts.owner.key(), ctx.accounts.main_data.owner);
+        ctx.accounts.organization_data.elections_count = 0;
+        Ok(())
+    }
+
+    pub fn create_election(ctx: Context<CreateElection>, input: ElectionData) -> Result<()> {
+        //TODO: validations:
+        //    * candidates length
+        //    * start in the future
+        //    * end after start
+        //TODO: payment 0.1 SOL
+        ctx.accounts.election_data.set_inner(input);
+        ctx.accounts.organization_data.elections_count += 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub owner: AccountInfo<'info>,
+    #[account(init, payer = owner, space = 8, seeds = [b"main_data"], bump)]
+    pub main_data: Account<'info, MainData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(_organization: Pubkey)]
+pub struct RegisterOrganization<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub owner: AccountInfo<'info>,
+    #[account(seeds = [b"main_data"], bump)]
+    pub main_data: Account<'info, MainData>,
+    #[account(
+        init, payer = owner, space = 8 + 16,
+        seeds = [&sha256(&format!("organization_data_{}", _organization))], 
+        bump
+    )]
+    pub organization_data: Account<'info, OrganizationData>,
+    pub system_program: Program<'info, System>,
+}
+
+fn sha256(value: &String) -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    hasher.update(value.as_bytes());
+    let result = hasher.finalize();
+    result.as_slice().try_into().expect("Wrong length")
+}
+
+#[derive(Accounts)]
+pub struct CreateElection<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub organization: AccountInfo<'info>,
+    #[account(mut, seeds = [b"main_data"], bump)]
+    pub main_data: Account<'info, MainData>,
+    #[account(
+        seeds = [format!("organization_data_{}", organization.key).as_bytes()], 
+        bump
+    )]
+    pub organization_data: Account<'info, OrganizationData>,
+    #[account(
+        init, payer = organization, space = 8, 
+        seeds = [format!("election_data_{}", organization_data.elections_count).as_bytes()], 
+        bump
+    )]
+    pub election_data: Account<'info, ElectionData>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MainData {
+    pub owner: Pubkey,
+}
+
+#[account]
+pub struct OrganizationData {
+    pub elections_count: u128, // 16
+}
+
+#[account]
+pub struct ElectionData {
+    start: i64,
+    end: i64,
+    title: String,
+    description: String,
+    candidates: Vec<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::sha256;
+
+    #[test]
+    fn playground() {
+        println!("Hash: {:?}", sha256(&"organization_data".to_string()));
+    }
+}
--- blockchain/solana/voting-poc/contract/tests/voting.ts
+++ blockchain/solana/voting-poc/contract/tests/voting.ts
@@ -1,17 +1,110 @@
 import * as anchor from "@project-serum/anchor";
+import {
+  PublicKey,
+  Keypair,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { sha256 } from "js-sha256";
+import { Voting } from "../target/types/voting";
 import idl from "../target/idl/voting.json";
 import secret from "../target/deploy/voting-keypair.json";
+import { before } from "mocha";
+import { expect } from "chai";
 const { Program, AnchorProvider } = anchor;
-const { Keypair } = anchor.web3;
 
 describe("voting", () => {
   anchor.setProvider(AnchorProvider.env());
+  const connection = anchor.getProvider().connection;
 
   const programId = Keypair.fromSecretKey(Uint8Array.from(secret)).publicKey;
-  const program = new Program(idl, programId);
+  const program = new Program(idl as any as Voting, programId);
 
-  it("Is initialized!", async () => {
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const owner = Keypair.generate();
+  const [mainData, _] = PublicKey.findProgramAddressSync(
+    [Buffer.from("main_data")],
+    programId
+  );
+  const systemProgram = SystemProgram.programId;
+
+  before(async () => {
+    console.log(`Owner: ${owner.publicKey}`);
+    await airdrop(owner.publicKey);
+    // await program.methods
+    //   .initialize()
+    //   .accounts({
+    //     owner: owner.publicKey,
+    //     mainData,
+    //     systemProgram,
+    //   })
+    //   .signers([owner])
+    //   .rpc();
   });
+
+  it("Should register organization", async () => {
+    // given
+    const organization = Keypair.generate();
+    console.log(`Organization: ${organization.publicKey}`);
+    await airdrop(organization.publicKey);
+    const organizationData = findPda(
+      sha256.array(`organization_data_${organization.publicKey}`)
+    );
+    console.log(`Organization Data: ${organizationData}`);
+
+    // when
+    const tx = await program.methods
+      .registerOrganization(organization.publicKey)
+      .accounts({
+        owner,
+        mainData,
+        systemProgram,
+        organizationData,
+      })
+      .signers([owner])
+      .rpc();
+    console.log(`Transaction: ${tx}`);
+
+    // then
+    const savedData = await program.account.organizationData.fetch(
+      organizationData
+    );
+    expect(savedData.electionsCount.toNumber()).to.equal(0);
+  });
+
+  it("Should forbid non-owner to register organizations", async () => {});
+
+  xit("Playground", () => {
+    // const firstPda = findPda("organization_data_1");
+    // console.log(`First PDA: ${firstPda}`);
+    // const secondPda = findPda("organization_data_2");
+    // console.log(`Second PDA: ${secondPda}`);
+    // expect(firstPda.toBase58()).to.not.equal(secondPda.toBase58());
+
+    const key = Keypair.generate().publicKey;
+    const hash = sha256.array(`organization_data_${key}`);
+    console.log(`Random PDA: ${findPda(hash)}`);
+
+    console.log(`Hash: ${sha256.array("organization_data")}`);
+  });
+
+  function findPda(seed: number[] | string): PublicKey {
+    return PublicKey.findProgramAddressSync([Buffer.from(seed)], programId)[0];
+  }
+
+  // TODO: check if needed for local env setup
+  async function airdrop(address: PublicKey): Promise<void> {
+    const txa = await connection.requestAirdrop(address, 2 * LAMPORTS_PER_SOL);
+    console.log(`Airdrop 2 SOL to ${address}: ${txa}`);
+    const latestBlockHash = await connection.getLatestBlockhash();
+    await connection.confirmTransaction({
+      signature: txa,
+      ...latestBlockHash,
+    });
+    console.log("Waiting for 10 seconds after airdrop...");
+    await wait(10000);
+  }
+
+  function wait(milliseconds: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  }
 });
